54e38d95fba7c57fcc92b5d0ba3ff2de2e25a2ab
BagusMiftah09 <bagusmiftah@prieds.com>
2024-11-06 20:02:33 +0800

    comment

--- utils/charges.go
+++ utils/charges.go
@@ -1,8 +1,12 @@
 package utils
 
 func CalculateCharge(hours int) int {
+
+	// kondisi dua jam pertama 10$
 	if hours <= 2 {
 		return 10
 	}
+
+	// dua jam setelahnya di tambahkan 10$
 	return 10 + (hours-2)*10
 }
